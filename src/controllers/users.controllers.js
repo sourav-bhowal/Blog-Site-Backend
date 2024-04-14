392a429212e3198629f7a25e93b318656a5cc3dc
Sourav Bhowal <souravbhowal15@gmail.com>
2024-04-14 20:30:37 +0530

    fix

--- src/controllers/users.controllers.js
+++ src/controllers/users.controllers.js
@@ -96,7 +96,8 @@ export const login = asyncHandler(async (req, res) => {
 
     const options = {
         httpOnly : true,
-        secure: true
+        secure: true,
+        sameSite: "none",
     };
 
     return res
