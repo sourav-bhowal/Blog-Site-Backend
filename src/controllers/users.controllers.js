bbfff488267416cc2cf2e5f8851a91b0d4a620d6
Sourav Bhowal <souravbhowal15@gmail.com>
2024-04-18 20:28:36 +0530

    fix

--- src/controllers/users.controllers.js
+++ src/controllers/users.controllers.js
@@ -131,7 +131,8 @@ export const logout = asyncHandler(async (req, res) => {
     const options = {
         httpOnly: true,
         secure: true,
-    }
+        sameSite: "none"
+    };
 
     return res
     .status(200)
